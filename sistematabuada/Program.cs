b83e5b8f2dc8b31ee7ad0c1c01adce2bd976d121
edsueco <eduardoalvesmelo@gmail.com>
2018-11-09 20:56:32 -0400

    first commit

---
+++ sistematabuada/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace sistematabuada
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Tabuada t1 = new Tabuada();
+            int i = 0;
+            while(i<4){
+                Console.WriteLine("MONTE SUA TABUADA");
+                Console.WriteLine("+ SOMAR");
+                Console.WriteLine("- SUBTRAIR");
+                Console.WriteLine("* MULTIPLICAR");
+                Console.WriteLine("/ DIVIDIR");
+                Console.Write("DIGITE A OPÇÃO DESEJADA: ");
+                t1.montaTabuada(Console.ReadLine());
+                Console.ReadKey();
+                Console.Clear();
+                i++;
+            }
+
+        }
+    }
+}
---
+++ sistematabuada/Tabuada.cs
@@ -0,0 +1,31 @@
+﻿using System;
+namespace sistematabuada
+{
+    public class Tabuada
+    {
+        //public string op { set; get; }
+
+        public void montaTabuada(string op)
+        {
+
+            for (int i = 1; i <= 9; i++)
+            {
+                //Console.WriteLine("Tabuada de {0}", i);
+
+                for (int j = 1; j <= 9; j++)
+                {
+                    if (op == "+")      { Console.Write("{0} + {1} = {2}\t", i, j, i + j); }
+                    else if (op == "-") { Console.Write("{0} - {1} = {2}\t", i, j, i - j); }
+                    else if (op == "*") { Console.Write("{0} * {1} = {2}\t", i, j, i * j); }
+                    else if (op == "/") { Console.Write("{0} / {1} = {2}\t", i, j, i / j); }
+                }
+                Console.WriteLine("\n");
+            }
+
+        }
+
+        public Tabuada()
+        {
+        }
+    }
+}
